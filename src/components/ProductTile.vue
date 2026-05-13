811f550278dfbf701ab99329b80a01d7dc03d7c8
lolba0198 <lolba0198@gmail.com>
2026-05-13 20:43:56 +0200

    Update ProductTile.vue

--- src/components/ProductTile.vue
+++ src/components/ProductTile.vue
@@ -1,20 +1,32 @@
 <template>
-  <RouterLink :to="`/product/${product.id}`">
-    <img :src="`/src/assets/${product.image}`" :alt="product.name" />
-    <div>{{ product.name }}</div>
-    <div>{{ product.price }}</div> 
+  <RouterLink :to="`/product/${product.id}`" class="tile-link">
+    <img :src="`/src/assets/${product.image}`" :alt="product.name" class="product-img" />
+    <h3 class="product-name">{{ product.name }}</h3>
+    
+    <AppRating 
+      v-if="product.ratingOption"
+      :rate="product.rate" 
+      :ratesNumber="product.ratesNumber" 
+    />
+    
+    <div class="product-price">{{ product.price }} zł</div>
   </RouterLink>
 </template>
 
 <script>
+import AppRating from '@/components/AppRating.vue';
+
 export default {
+  components: { AppRating },
   props: {
-    product: {
-      type: Object,
-      required: true
-    }
+    product: Object
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.tile-link { text-decoration: none; color: #333; display: block; }
+.product-img { width: 100%; height: 200px; object-fit: cover; }
+.product-name { margin: 10px 0; font-size: 1.2rem; }
+.product-rating { color: #f39c12; }
+</style>
