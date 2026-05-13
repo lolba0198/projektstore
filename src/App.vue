34f153ec001b90912a9a171843db26f3977f9161
lolba0198 <lolba0198@gmail.com>
2026-05-13 20:45:19 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <Header></Header>
-    <RouterView />
+    <AppLoader v-if="promotionLoading" /> <RouterView />
   </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import Header from './components/Header.vue';
+import AppLoader from './components/AppLoader.vue';
 
+const store = useStore();
+const promotionLoading = computed(() => store.getters.GET_PROMOTION_LOADING);
 </script>
