a71e698bce4bd9ef314985ff9a1d527f8904806d
lolba0198 <lolba0198@gmail.com>
2026-05-14 16:44:31 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore } from 'vuex'
 
 import promotions from './promotions'
 import promotion from './promotion'
+import user from './user'
 
 const modules = {
-    promotions, promotion
+    promotions, promotion, user
     // kolejne moduły
 
 }
