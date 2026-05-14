ad5e6bc4b50413a1395caa930c63befcea6eff84
lolba0198 <lolba0198@gmail.com>
2026-05-14 16:46:07 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
+    <AppLoader v-if="isLoading" />
     <Header></Header>
-    <AppLoader v-if="promotionLoading" /> <RouterView />
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import Header from './components/Header.vue';
 import AppLoader from './components/AppLoader.vue';
 
 const store = useStore();
-const promotionLoading = computed(() => store.getters.GET_PROMOTION_LOADING);
+
+const isLoading = computed(() => {
+  return (
+    store.getters['GET_PROMOTIONS_LOADING'] || 
+    store.getters['GET_PROMOTION_LOADING'] ||
+    store.getters['GET_CURRENT_USER_LOADING'] 
+  );
+});
+onMounted(() => {
+  store.dispatch("FETCH_CURRENT_USER");
+});
 </script>
