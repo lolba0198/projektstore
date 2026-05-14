e6a6afda04144c5e8d08473cc3cdf22b16dcb261
lolba0198 <lolba0198@gmail.com>
2026-05-14 16:43:52 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,100 @@
 <template>
     <header>
         <div class="link-container">
-            <RouterLink class="hbuttons" to="/" exact>Home</RouterLink>
-            <RouterLink class="hbuttons" to="/about">About</RouterLink>
-            <RouterLink class="hbuttons" to="/nnn">Not found</RouterLink>
+            <div class="nav-links">
+                <RouterLink class="hbuttons" to="/" exact>Home</RouterLink>
+                <RouterLink class="hbuttons" to="/about">About</RouterLink>
+                <RouterLink class="hbuttons" to="/nnn">Not found</RouterLink>
+            </div>
+            
+            <div class="auth-links">
+                <span v-if="user" class="user-info">Witaj, {{ user.email }}</span>
+
+                <template v-if="!user">
+                    <RouterLink class="hbuttons" to="/login">Login</RouterLink>
+                    <RouterLink class="hbuttons" to="/register">Register</RouterLink>
+                </template>
+
+                <button v-else @click="logout" class="hbuttons logout-btn">Logout</button>
+            </div>
         </div>
     </header>
 </template>
 
 <script>
-import { RouterLink } from 'vue-router';
+export default {
+    name: "Header",
+    computed: {
+        user() {
+            return this.$store.getters["GET_CURRENT_USER"];
+        },
+        userLoading() {
+            return this.$store.getters["GET_CURRENT_USER_LOADING"];
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch("LOGOUT_USER")
+                .then(() => {
+                    this.$router.push("/login");
+                });
+        }
+    }
+}
 </script>
 
 <style scoped>
-.hbuttons {
-    margin-right: 50px;
-    font-size: 30px;
-    text-decoration: none;
-    color: white;
-    padding: 15px;
-    margin-top: 3px;
-    font-family: 'Roboto', sans-serif;
-}
-
-.hbuttons:hover {
-    background-color: lightskyblue;
-    margin-bottom: 3px;
-    transition: ease-in 0.08s;
-} 
-
-
 .link-container {
     background-color: rgb(53, 53, 245);
     height: 68px;
     width: 100vw;
     margin-left: -8px;
     display: flex;
+    justify-content: space-between; 
+    align-items: center;
+}
 
+.nav-links, .auth-links {
+    display: flex;
+    align-items: center;
+}
+
+.hbuttons {
+    font-size: 30px;
+    text-decoration: none;
+    color: white;
+    padding: 15px;
+    font-family: 'Roboto', sans-serif;
+    display: inline-block;
+    height: 38px;
+    line-height: 38px;
+}
+
+.nav-links .hbuttons {
+    margin-right: 10px;
+}
+
+.hbuttons:hover {
+    background-color: lightskyblue;
+    transition: ease-in 0.08s;
+}
+
+.auth-links {
+    margin-right: 20px;
+}
+
+.user-info {
+    color: white;
+    font-size: 20px;
+    margin-right: 15px;
+    font-family: 'Roboto', sans-serif;
+}
+
+.logout-btn {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 30px;
+    color: white;
 }
 </style>
